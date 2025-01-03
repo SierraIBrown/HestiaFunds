8b02c9950465a78c9fe2ac56fcbce09462890ffb
Sierra (Izumi) Brown <sib37451@gmail.com>
2025-01-02 20:12:38 -0600

    Initial commit with Spring Boot setup

---
+++ HestiaFundsBackend/src/main/java/com/SierraIBrown/HestiaFundsBackend/HestiaFundsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.SierraIBrown.HestiaFundsBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HestiaFundsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HestiaFundsBackendApplication.class, args);
+	}
+
+}
