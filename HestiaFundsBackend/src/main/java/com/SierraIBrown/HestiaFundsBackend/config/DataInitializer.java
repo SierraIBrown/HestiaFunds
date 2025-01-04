2e3686eea44bfffddeb1a145230b01b01adb825b
Sierra (Izumi) Brown <sib37451@gmail.com>
2025-01-03 18:46:32 -0600

    Quick GET and POST fixes

--- HestiaFundsBackend/src/main/java/com/SierraIBrown/HestiaFundsBackend/config/DataInitializer.java
+++ HestiaFundsBackend/src/main/java/com/SierraIBrown/HestiaFundsBackend/config/DataInitializer.java
@@ -1,2 +1,39 @@
-package com.SierraIBrown.HestiaFundsBackend.config;public class DataInitializer {
+package com.SierraIBrown.HestiaFundsBackend.config;
+
+import com.SierraIBrown.HestiaFundsBackend.model.Category;
+import com.SierraIBrown.HestiaFundsBackend.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class DataInitializer {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @EventListener
+    public void onApplicationReady(ApplicationReadyEvent event){
+        long categoryCount = categoryRepository.count();
+        if(categoryCount == 0){
+            List<Category> defaultCategories = Arrays.asList(
+                    new Category("Car Loans"),
+                    new Category("Credit Card Bills"),
+                    new Category("Gas"),
+                    new Category("Groceries"),
+                    new Category("Entertainment"),
+                    new Category("Personal"),
+                    new Category("Rent/Mortgage"),
+                    new Category("Student Loans"),
+                    new Category("Utilties")
+            );
+
+            categoryRepository.saveAll(defaultCategories);
+            System.out.println("Default categories have been inserted");
+        }
+    }
 }
--- HestiaFundsBackend/src/main/java/com/SierraIBrown/HestiaFundsBackend/repository/CategoryRepository.java
+++ HestiaFundsBackend/src/main/java/com/SierraIBrown/HestiaFundsBackend/repository/CategoryRepository.java
@@ -1,2 +1,12 @@
-package com.SierraIBrown.HestiaFundsBackend.repository;public class CategoryRepository {
+package com.SierraIBrown.HestiaFundsBackend.repository;
+
+import com.SierraIBrown.HestiaFundsBackend.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    List<Category> findAllByOrderByNameAsc();
 }
