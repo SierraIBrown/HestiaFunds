49a2eac7b1fddd80c6d69ec88e205d5bc02acaf7
Sierra (Izumi) Brown <sib37451@gmail.com>
2025-01-15 21:08:36 -0600

    Budgets frontend created

---
+++ HestiaFundsFrontend/.vscode/js/budgets.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const API_BASE_URL = "http://localhost:8080/api";
+
+    const budgetsTableBody = document.getElementById("budget-table-body");
+    const saveBudgetsButton = document.getElementById("save-budgets");
+    const viewEditCategoriesButton = document.getElementById("view-edit-categories");
+    const backToTransactionsButton = document.getElementById("back-to-transactions");
+
+    
+})
